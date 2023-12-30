fb83f7625bff339edf7c4c06d6718f41b730583f
Wendy Ndlalane <wendy.96.wa@gmail.com>
2023-12-30 11:13:21 +0200

    Adicao de rotas para o retorno dos dados

--- index.js
+++ index.js
@@ -4,19 +4,68 @@ const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
 const PORT = 3001;
+app.use(bodyParser.json());
 
 const supabaseUrl = 'https://vdeaiezweqmvfbzifbxu.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkZWFpZXp3ZXFtdmZiemlmYnh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM3MDM1MTYsImV4cCI6MjAxOTI3OTUxNn0.Q4g6YkF_hX_18pZuHv2B2OX8RyM_EuDua7CDAIlTado';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+// Instruções para uso da API
+rotas = [
+    {
+      nome: "clientes",
+      metodo: "GET",
+      descricao: "Obtém todos os clientes",
+    },
+    {
+      nome: "tecnicos",
+      metodo: "GET",
+      descricao: "Obtém todos os técnicos",
+    },
+    {
+      nome: "tickets",
+      metodo: "GET",
+      descricao: "Obtém todos os tickets",
+    },
+    {
+      nome: "tickets",
+      metodo: "POST",
+      descricao: "Cria um novo ticket",
+    },
+  ];
+  
 
-app.use(bodyParser.json());
+app.get("/", (req, res) => {
+    res.json(rotas);
+  });
+
+// Rota para obter todos os Clientes
+app.get('/clientes', async (req, res) => {
+    const { data, error } = await supabase.from('usuario').select().eq('cliente', true);
+        
+    if (error) {
+        return res.status(500).json({ error: 'Error fetching tickets from Superbase' });
+    }
+
+    res.json(data);
+});
+
+// Rota para obter todos os Tecnicos
+app.get('/tecnicos', async (req, res) => {
+    const { data, error } = await supabase.from('usuario').select().eq('tecnico', true);
+        
+    if (error) {
+        return res.status(500).json({ error: 'Error fetching tickets from Superbase' });
+    }
+
+    res.json(data);
+});
 
 // Rota para obter todos os tickets
 app.get('/tickets', async (req, res) => {
-    const { data, error } = await supabase.from('Ticket').select();
-
+    const { data, error } = await supabase.from('ticket').select();
+        
     if (error) {
         return res.status(500).json({ error: 'Error fetching tickets from Superbase' });
     }
@@ -32,7 +81,7 @@ app.post('/tickets', async (req, res) => {
         return res.status(400).json({ error: 'Assunto e descrição são obrigatórios' });
     }
 
-    const { data, error } = await supabase.from('Ticket').insert([
+    const { data, error } = await supabase.from('ticket').insert([
         {
             assunto,
             descricao,
